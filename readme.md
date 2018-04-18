9a003dc8e1e1e383683467beaf6c31311fc557b1
Joël Cohen <j03l.c0h3n@gmail.com>
2018-04-18 17:19:09 +0200

    version initiale

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Séance 1
+
+Cette première séance est une séance de révision autour des notions de variables ainsi que des condition et boucles. Vous trouverez un fichier `variables.py` à completer.
---
+++ variables.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+###############################
+# Exercices sur les variables #
+###############################
+
+
+# Savoirs-faires :
+#    créer une variable et lui affecter une valeur
+#    afficher des variables
+#    faire des opérations sur des variables
+
+# --------
+# Niveau 1
+# --------
+
+# Exercice 1.1
+#
+#   Définir la bonne variable pour faire fonctionner le code ci-dessous.
+#
+#   Savoirs-faires :
+#       créer une variable et lui affecter une valeur
+
+# ... Écrivez votre code ici ...
+
+print(ma_variable)
+
+
+
+# Exercice 1.2
+#
+#   Le code ci-dessous définit un variable contenant la chaîne de caractères "Salut !". Pour le moment rien ne s'affiche. Afficher la variable en utilisant la fonction print().
+#
+#   Savoirs-faires :
+#       afficher des variables
+
+# ... Écrivez votre code ici ...
+
+texte = "Salut !"
+
+
+
+# Exercice 1.3
+#
+#   Le code ci-dessous initialise une variable à la valeur 2 puis l'affiche. Ajouter une ligne de code pour augmenter la valeur de la variable de 3 (sans toucher à la ligne d'initialisation !)
+#
+#   Savoirs-faires :
+#       faire des opérations sur des variables
+
+
+# ... Écrivez votre code ici ...
+
+nombre = 2
+
+print(nombre)
+
+
+# Exercice 1.4
+#
+#     Définir une variable appelée nombre_de_clients à la valeur 0 (aucun client au départ). Ajouter 2 puis retrancher 1 (2 clients entrent, puis un client sort). Afficher le nombre de clients restants.
+#
+#     Savoirs-faires :
+#         créer un variable et lui affecter une valeur
+#         faire des opérations sur des variables
+#         afficher des variables
+
+
+# ... Écrivez votre code ici ...
+
+
+
